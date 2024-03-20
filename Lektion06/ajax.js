abe1b220b7a939cd3029a396a57599fe496c2bf1
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2024-03-20 09:54:30 +0100

    Lektion 6 - Ajax

---
+++ Lektion06/ajax.js
@@ -0,0 +1,49 @@
+/**
+ * 
+ *  Ajax Demo
+ *  2024-03-20
+ * 
+ */
+
+let loadBtn = document.getElementById('loadBtn');
+let loadJsonBtn = document.getElementById('loadJSON');
+
+// Skapa en händelsehanterare (lyssnare) på knappen 
+loadBtn.addEventListener('click', loadText);
+loadJsonBtn.addEventListener('click', loadJSON)
+
+function loadText() {
+    console.log("Inne i Ajax-funktionen");
+    const xhr = new XMLHttpRequest();
+    // console.log(xhr);
+    xhr.onload = function () {
+        console.log("Inne i onload");
+        console.log(xhr);
+
+        document.getElementById('output').innerHTML = xhr.responseText;
+    };
+    xhr.open('GET', 'demo.html');
+    xhr.send();
+}
+
+function loadJSON() {
+    console.log("Inne i loadJSON-funktionen");
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        console.log(xhr.response);
+        const json = JSON.parse(xhr.response);
+        console.log(json);
+        renderJSON(json);
+    };
+    xhr.open('GET', 'demo.json');
+    xhr.send();
+}
+
+
+function renderJSON(json) {
+    document.getElementById('output').innerHTML = `
+    <h2>${json.name}</h2>
+    <h3>${json.email}</h3>
+    <h4>Company: ${json.company.name}</h4>
+    `;
+}
