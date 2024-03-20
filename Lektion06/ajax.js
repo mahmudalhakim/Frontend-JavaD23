6039465dcf620bed7aaf654fb0c75e0e92c1861d
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2024-03-20 10:50:41 +0100

    Lektion 6 - jQuery

--- Lektion06/ajax.js
+++ Lektion06/ajax.js
@@ -6,11 +6,13 @@
  */
 
 let loadBtn = document.getElementById('loadBtn');
-let loadJsonBtn = document.getElementById('loadJSON');
+let loadJsonBtn = document.getElementById('loadJsonBtn');
+let loadApiBtn = document.getElementById('loadApiBtn');
 
-// Skapa en händelsehanterare (lyssnare) på knappen 
+// Skapa händelsehanterare (lyssnare)  
 loadBtn.addEventListener('click', loadText);
 loadJsonBtn.addEventListener('click', loadJSON)
+loadApiBtn.addEventListener('click', loadApi)
 
 function loadText() {
     console.log("Inne i Ajax-funktionen");
@@ -47,3 +49,43 @@ function renderJSON(json) {
     <h4>Company: ${json.company.name}</h4>
     `;
 }
+
+
+function loadApi() {
+    console.log("Inne i loadApi-funktionen");
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+    xhr.send();
+    xhr.onload = function () {
+        // console.log(xhr.response);
+        const json = JSON.parse(xhr.response);
+        console.log(json);
+        renderApi(json);
+    };
+}
+
+
+function renderApi(json) {
+
+    let output = '<ol>';
+    json.forEach(user => {
+        output += '<li>' + user.name + '</li>';
+    });
+    output += '</ol>';
+
+    document.getElementById('output').innerHTML = output;
+}
+
+
+
+// Ajax via jQuery
+$(document).ready(function(){
+    // Hämta text/html
+    $('#output-jquery').load('demo.html');
+
+    // Hämta JSON
+    $.getJSON('demo.json', function(response){
+        console.log(response);
+        $('#output-jquery-json').html(response.name);
+    })
+});
